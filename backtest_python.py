3e86980f654feaddba042837028c2439099e53e8
Lancelot <122176809+Lancelot65@users.noreply.github.com>
2023-07-22 00:01:55 +0200

    save

--- backtest_python.py
+++ backtest_python.py
@@ -11,6 +11,12 @@ class Backtest:
 		self.positions_short = []
 		self.quantite_position_short = []
 
+		self.trade_short_pc = []
+		self.trade_long_pc = []
+
+		self.trade_short_v = []
+		self.trade_long_v = []
+
 
 	def load_data(self, symbol='BTC/USDT', time='30m', length=500):
 		#telecharcgement des donnée ohlcv
@@ -43,20 +49,25 @@ class Backtest:
 		if self.positions_long:
 			for pos in range(len(self.positions_long)):
 				self.capital += (close - self.positions_long[pos]) * self.quantite_position_long[pos] # - (self.positions_long[pos] * self.quantite_position_long[pos] * 0.3
+				
+				self.trade_long_pc.append(((close / self.positions_long[pos]) - 1) * 100)
+				self.trade_long_v.append((close - self.positions_long[pos]) * self.quantite_position_long[pos])
+			self.append_element_df("long_close", position)
+
+
 			self.positions_long.clear()
 			self.quantite_position_long.clear()
-			self.append_element_df("long_close", position)
-			
+			"""
 			if test == 1:
 				print("close garce au take_profit", close, "   ", position)
 			elif test == 2:
 				print("close garce au stop_loss", close, "   ", position)
 			elif test == 0:
-				print("close_short")
+				print("close_long")
 				print(close, "   ", position)
 			else:
 				print("choix_incorrect", test)
-		
+			"""
 	def open_short(self, close, pos, quantite=0.1):
 		if not self.positions_short:
 			self.positions_short.append(close)
@@ -68,9 +79,15 @@ class Backtest:
 		if self.positions_short:
 			for pos in range(len(self.positions_short)):
 				self.capital += (self.positions_short[pos] - close)  * self.quantite_position_short[pos] # - (self.positions_short[pos] * self.quantite_position_short[pos] * 0.3)
+				self.trade_short_pc.append(((self.positions_short[pos] / close) - 1) * 100)
+				self.trade_short_v.append((self.positions_short[pos] - close) * self.quantite_position_short[pos])
+
+			self.append_element_df("short_close", position)
+			
+
 			self.positions_short.clear()
 			self.quantite_position_short.clear()
-			self.append_element_df("short_close", position)
+			"""
 			
 			if test == 1:
 				print("close garce au take_profit", close, "   ", position)
@@ -80,8 +97,8 @@ class Backtest:
 				print("close_short", close, "   ", position)
 			else:
 				print("choix_incorrect", test)
+			"""
 				
-	
 	def show_evolution_price(self):
 		self.data.evolution_price.plot()
 	
@@ -97,7 +114,6 @@ class Backtest:
 		if self.positions_short:
 			print("***  ", ((self.positions_short[0] / close) - 1) * 100, "  ***")
 		
-	
 	def take_profit(self, close, position):
 		if self.positions_long:
 			
@@ -126,13 +142,42 @@ class Backtest:
 				print("stop_loss_short")
 				print(((close / self.positions_short[0]) - 1) * 100)
 				self.close_short(close, position, 2)
-
-	def look_money(self):
-		print(f"Votre porte feuille contient {self.capital}$")
 	
-	def rendement_final(self):
+	def stat_final(self):
 		self.rendement = ((self.capital - self.capital_initial) / self.capital_initial) * 100
-		print(f"Le taux d'évolution est de {self.rendement}$")
+		print(f"Votre capital a évoluer de {self.rendement}%, avec une valeur initiale de {self.capital_initial}$ à {self.capital}$ soit une évolution de {self.capital_initial - self.capital}$")
+
+		print(f"total trade : {len(self.trade_long_v) + len(self.trade_short_v)}")
+		print(f"nombre long_trade : {len(self.trade_long_v)}")
+		print(f"nombre short_trade : {len(self.trade_short_v)}")
+
+		a, b = 0, 0
+		for element in self.trade_long_v:
+			if element > 0:
+				a +=1
+			elif element < 0:
+				b += 1
+		print(f"trade_long_win : {a}")
+		print(f"trade_long_lose : {b}")
+
+		a, b = 0, 0
+		for element in self.trade_short_v:
+			if element > 0:
+				a +=1
+			elif element < 0:
+				b += 1
+		print(f"trade_short_win : {a}")
+		print(f"trade_short_lose : {b}")
+
+		print(f"moyenne long_trade : {sum(self.trade_long_v) / len(self.trade_long_v)}$ / {sum(self.trade_long_pc) / len(self.trade_long_pc)}%")
+		print(f"moyenne short_trade : {sum(self.trade_short_v) / len(self.trade_short_v)}$ / {sum(self.trade_short_pc) / len(self.trade_short_pc)}%")
+
+		print(f"best long_trade : {max(self.trade_long_v)}$ / {max(self.trade_long_pc)}%")
+		print(f"best short_trade : {max(self.trade_short_v)}$ / {max(self.trade_short_pc)}%")
+
+		print(f"bad long_trade : {min(self.trade_long_v)}$ / {min(self.trade_long_pc)}%")
+		print(f"bad short_trade : {min(self.trade_short_v)}$ / {min(self.trade_short_pc)}%")
+
 	
 	def append_element_df(self, style:str, position):
 		"""
@@ -156,10 +201,10 @@ class Backtest:
 	
 	def graphique(self, list_indicateur:list):
 		#lsit_indicateur doit contenir les df de tout les indicateur utiliser
-		positions_so = list(np.where(self.data.position_short_open == True)[0])
-		positions_sc = list(np.where(self.data.position_short_close == True)[0])
-		positions_lo = list(np.where(self.data.position_long_open == True)[0])
-		positions_lc = list(np.where(self.data.position_long_close == True)[0])
+		positions_so = np.where(self.data.position_short_open == True)[0]
+		positions_sc = np.where(self.data.position_short_close == True)[0]
+		positions_lo = np.where(self.data.position_long_open == True)[0]
+		positions_lc = np.where(self.data.position_long_close == True)[0]
 
 		for indicateur in list_indicateur:
 			indicateur.plot()
